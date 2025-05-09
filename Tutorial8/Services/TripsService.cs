d348751f3a481a1f9013a7027acf7929e83d1467
Jakub Rawski <kuba.s.rawski@gmail.com>
2025-05-09 14:25:23 +0200

    dodano nowe koncowki

--- Tutorial8/Services/TripsService.cs
+++ Tutorial8/Services/TripsService.cs
@@ -3,36 +3,77 @@ using Tutorial8.Models.DTOs;
 
 namespace Tutorial8.Services;
 
-public class TripsService : ITripsService
+public class TripsService(IConfiguration config) : ITripsService
 {
-    private readonly string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=APBD;Integrated Security=True;";
-    
+//"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Database;Integrated Security=True;";
+
+    private readonly string? _connectionString = config.GetConnectionString("Default");
     public async Task<List<TripDTO>> GetTrips()
     {
         var trips = new List<TripDTO>();
+        using var conn = new SqlConnection(_connectionString);
+        await conn.OpenAsync();
 
-        string command = "SELECT IdTrip, Name FROM Trip";
-        
-        using (SqlConnection conn = new SqlConnection(_connectionString))
-        using (SqlCommand cmd = new SqlCommand(command, conn))
+        var cmd = new SqlCommand(@"
+            SELECT t.IdTrip, t.Name, c.Name AS Country
+            FROM Trip t
+            JOIN Country_Trip ct ON t.IdTrip = ct.IdTrip
+            JOIN Country c ON ct.IdCountry = c.IdCountry", conn);
+
+        using var reader = await cmd.ExecuteReaderAsync();
+        var tripDict = new Dictionary<int, TripDTO>();
+
+        while (await reader.ReadAsync())
         {
-            await conn.OpenAsync();
+            int idTrip = reader.GetInt32(0);
+            string tripName = reader.GetString(1);
+            string country = reader.GetString(2);
 
-            using (var reader = await cmd.ExecuteReaderAsync())
+            if (!tripDict.TryGetValue(idTrip, out var trip))
             {
-                while (await reader.ReadAsync())
-                {
-                    int idOrdinal = reader.GetOrdinal("IdTrip");
-                    trips.Add(new TripDTO()
-                    {
-                        Id = reader.GetInt32(idOrdinal),
-                        Name = reader.GetString(1),
-                    });
-                }
+                trip = new TripDTO { Id = idTrip, Name = tripName, Countries = new List<CountryDTO>() };
+                tripDict[idTrip] = trip;
             }
+            trip.Countries.Add(new CountryDTO { Name = country });
         }
-        
 
-        return trips;
+        return tripDict.Values.ToList();
     }
+
+    public async Task<List<TripDTO>> GetTripsForClient(int clientId)
+    {
+        var trips = new List<TripDTO>();
+        using var conn = new SqlConnection(_connectionString);
+        await conn.OpenAsync();
+
+        var cmd = new SqlCommand(@"
+            SELECT t.IdTrip, t.Name, c.Name AS Country
+            FROM Client_Trip ct
+            JOIN Trip t ON ct.IdTrip = t.IdTrip
+            JOIN Country_Trip ctr ON t.IdTrip = ctr.IdTrip
+            JOIN Country c ON ctr.IdCountry = c.IdCountry
+            WHERE ct.IdClient = @id", conn);
+        cmd.Parameters.AddWithValue("@id", clientId);
+
+        using var reader = await cmd.ExecuteReaderAsync();
+        var tripDict = new Dictionary<int, TripDTO>();
+
+        while (await reader.ReadAsync())
+        {
+            int idTrip = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            string country = reader.GetString(2);
+
+            if (!tripDict.TryGetValue(idTrip, out var trip))
+            {
+                trip = new TripDTO { Id = idTrip, Name = name, Countries = new List<CountryDTO>() };
+                tripDict[idTrip] = trip;
+            }
+            trip.Countries.Add(new CountryDTO { Name = country });
+        }
+
+        return tripDict.Values.ToList();
+    }
+
+    
 }
